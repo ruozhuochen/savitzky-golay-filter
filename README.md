2d9a0308277741f6e554f91bc7e29055e87339f3
Roger Chen <rchen@buildingsalive.com>
2019-11-29 13:40:27 +1100

    refactor

---
+++ README.md
@@ -0,0 +1,10 @@
+# Savitzky Golay Filter
+
+## Purpose
+This project is based on https://github.com/swallez/savitzky-golay-filter, 
+which originated from https://code.google.com/p/savitzky-golay-filter
+
+## Modification 
+Modified with code from https://github.com/swallez/savitzky-golay-filter issues, which an user has provided an 
+modified implementation based common-math3 instead of the outdated common-math. 
+
---
+++ src/test/java/com/rogerose/os/math/sgfilter/SGFilterTest.java
@@ -0,0 +1,28 @@
+package com.rogerose.os.math.sgfilter;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class SGFilterTest {
+
+    @Test
+    void smooth() {
+        List<Double> data = Arrays.asList(2.0, 4.0, 6.0, 2.0, 2.0, 2.0, 2.0, 3.0);
+        int order = 1;
+        int nl = data.size() / 2;
+        int nr = data.size() - nl;
+        double[] coefficients = SGFilter.computeSGCoefficients(nl, nr, order);
+        double[] output = new SGFilter(nl, nr).smooth(data.stream().mapToDouble(Double::doubleValue).toArray(), coefficients);
+        System.out.println(Arrays.stream(output).boxed().collect(Collectors.toList()));
+    }
+}
